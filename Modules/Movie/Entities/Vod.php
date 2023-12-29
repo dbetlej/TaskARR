7162052644abbec3d8ee86c25346518e5bff1eb6
dbetlej <dominik.filip.betlej@gmail.com>
2023-12-29 22:09:05 +0100

    fix vod user info

--- Modules/Movie/Entities/Vod.php
+++ Modules/Movie/Entities/Vod.php
@@ -5,6 +5,7 @@ namespace Modules\Movie\Entities;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
@@ -28,7 +29,6 @@ class Vod extends Model
         'url',
         'short_description',
         'description',
-        'favorite',
         'price',
         'bought'
     ];
@@ -64,6 +64,27 @@ class Vod extends Model
     public function tagable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_movie')
+            ->withPivot('ignore','favorite','to_watch','wish_list','position')
+            ->withTimestamps();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function user(): mixed
+    {
+        return $this->users()
+            ->where('user_id', auth()->id())
+            ->withPivot('ignore','favorite','to_watch','watched','position')
+            ->first();
     }
 
     /**
